dcec11b294e091d4c80dee27dbabcc5888978f21
Jonghun Lim <jhlim00812@gmail.com>
2026-05-12 18:12:49 +0900

    Chore: 미사용 import문 제거

--- src/main/java/com/ceos/vote/global/jwt/filter/JwtTokenFilter.java
+++ src/main/java/com/ceos/vote/global/jwt/filter/JwtTokenFilter.java
@@ -3,7 +3,6 @@ package com.ceos.vote.global.jwt.filter;
 import java.io.IOException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
