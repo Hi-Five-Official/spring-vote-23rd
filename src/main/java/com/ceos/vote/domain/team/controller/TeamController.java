71aa861c0229fb48f5bba7adeacb70515a01afe3
fervovita <rrvvy9595@gmail.com>
2026-05-17 03:31:12 +0900

    Refactor(candidate): 팀별 팀원 조회 API를 TeamController로 이동

--- src/main/java/com/ceos/vote/domain/team/controller/TeamController.java
+++ src/main/java/com/ceos/vote/domain/team/controller/TeamController.java
@@ -1,12 +1,17 @@
 package com.ceos.vote.domain.team.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ceos.vote.domain.candidate.dto.response.CandidateListResponse;
+import com.ceos.vote.domain.candidate.service.CandidateService;
 import com.ceos.vote.domain.team.dto.response.TeamListResponse;
 import com.ceos.vote.domain.team.service.TeamService;
 import com.ceos.vote.global.apipayload.response.ApiResponse;
+import com.ceos.vote.global.entity.enums.Part;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,12 +24,24 @@ import lombok.RequiredArgsConstructor;
 public class TeamController {
 
 	private final TeamService teamService;
+	private final CandidateService candidateService;
 
 	@Operation(summary = "팀 조회", description = "팀 이름을 조회합니다.")
 	@GetMapping
 	public ApiResponse<TeamListResponse> getTeams() {
 		TeamListResponse response = teamService.getTeams();
 		return ApiResponse.onSuccess("팀 조회 성공", response);
+	}
+
+	@Operation(summary = "팀별 팀원 조회", description = "각 팀에 속한 팀원을 조회합니다.")
+	@GetMapping("/{teamId}/candidates")
+	public ApiResponse<CandidateListResponse> getTeamCandidates(
+		@PathVariable Long teamId,
+		@RequestParam Part part
+	) {
+		CandidateListResponse response = candidateService.getByTeamIdAndPart(teamId, part);
+
+		return ApiResponse.onSuccess("팀별 팀원 조회 성공", response);
 	}
 
 }
